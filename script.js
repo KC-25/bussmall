9c2833a6339554b433114bf68797a84036d34007
KC-25 <kierrconnell@g.crookcountyschools.org>
2023-04-19 14:23:07 -0700

    have no idea wht im doing

---
+++ script.js
@@ -0,0 +1,23 @@
+
+'use strict'
+
+console.log("Hello world");
+
+const div = document.getElementsByTagName('div');
+const img1 = document.getElementById("i1");
+const img2 = document.getElementById("i2");
+const img3 = document.getElementById("i3");
+
+const shark = document.getElementById('i1');
+const dog = document.getElementById('i2');
+shark.src = './img/shark.jpg';
+dog.src = './img/dog-duck.jpg';
+
+this.img = (shark);
+this.imgpath = (dog);
+this.clicks = 0;
+this.views = 0;
+
+array = [];
+
+

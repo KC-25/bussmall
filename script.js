4439744fa0edbae58f479184d51ac2481f42e82c
KC-25 <kierrconnell@g.crookcountyschools.org>
2023-04-21 14:04:09 -0700

    put all images in array

--- script.js
+++ script.js
@@ -3,21 +3,37 @@
 
 console.log("Hello world");
 
-const div = document.getElementsByTagName('div');
+const imageContainer = document.getElementsByTagName('ads');
 const img1 = document.getElementById("i1");
 const img2 = document.getElementById("i2");
 const img3 = document.getElementById("i3");
+const T1 = document.getElementById("t1");
+const T2 = document.getElementById("t2");
+const T3 = document.getElementById("t3");
 
-const shark = document.getElementById('i1');
-const dog = document.getElementById('i2');
-shark.src = './img/shark.jpg';
-dog.src = './img/dog-duck.jpg';
+function image (name, imgsrc){
+    this.name = name;
+    this.imgsrc = imgsrc;
+    this.clicks = 0;
+    this.views = 0;
+};
 
-this.img = (shark);
-this.imgpath = (dog);
-this.clicks = 0;
-this.views = 0;
+image.nameofarray = [];
 
-array = [];
+image.nameofarray.push ('bag', './images/bag.jpg');
+image.nameofarray.push ('banana', './images/banana.jpg');
+image.nameofarray.push ('bathroom', './images/bathroom.jpg');
+image.nameofarray.push ('boots', './images/boots.jpg');
+image.nameofarray.push ('bubblegum', './images/bubblegum.jpg');
+image.nameofarray.push ('chair', './images/chair.jpg');
+image.nameofarray.push ('cthulu', './images/cthulhu.jpg');
+image.nameofarray.push ('dragon', './images/dragon.jpg');
+image.nameofarray.push ('pen', './images/pen.jpg');
+image.nameofarray.push ('pet-sweep', './images/pet-sweep.jpg');
+image.nameofarray.push ('scissors', './images/scissors.jpg');
+image.nameofarray.push ('sweep', './images/sweep.jpg');
+image.nameofarray.push ('tauntaun', './images/tauntaun.jpg');
+image.nameofarray.push ('water-can', './images/water-can.jpg');
+image.nameofarray.push ('wine-glass', './images/wine-glass.jpg');
 
-
+console.log(image.nameofarray);
